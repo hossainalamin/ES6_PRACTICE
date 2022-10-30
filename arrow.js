065a0fe30832e476dceb95688cd866dc9643ff41
hossainalamin <hossainalamin980@gmail.com>
2022-10-30 23:23:43 +0600

    first day

---
+++ arrow.js
@@ -0,0 +1,15 @@
+//Function expression
+// const calcNum = function(num)
+// {
+//     return num*2;
+// }
+const calcNumber = (num,muliplier) => num*muliplier;
+console.log(calcNumber(4,10));
+
+const exampleFunction = (a,b) =>{
+    const numOne = a;
+    const numTwo = b
+    return a+b
+}
+
+console.log(exampleFunction(20,30))
---
+++ template.js
@@ -0,0 +1,6 @@
+let name = "Alamin Hossain";
+var age = "23";
+var fullInfo = `${name} ${age}`;
+//console.log(fullInfo);
+name = "Rokib";
+console.log(name);
